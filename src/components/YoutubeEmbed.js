bb58a81ffe4922f2c199cd1f287c7bdda480faf5
Gedeonir <irafasha.jedy12@gmail.com>
2023-08-11 23:01:45 +0200

    Added admin pages

--- src/components/YoutubeEmbed.js
+++ src/components/YoutubeEmbed.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 const YoutubeEmbed = ({ embedId }) => (
     <div className="w-full lg:px-4 py-4">
       <iframe
-        className='w-full h-56'
+        className='w-full lg:h-96 h-56'
         src={`https://www.youtube.com/embed/${embedId}`}
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
