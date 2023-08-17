d276d199935f622fe9b155d607772e9f43937af4
Gedeonir <irafasha.jedy12@gmail.com>
2023-08-17 10:33:05 +0200

    deployment Fixes

--- src/redux/Actions/axiosConfig.js
+++ src/redux/Actions/axiosConfig.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 
-const token = sessionStorage.getItem('memberToken').replace(/"/g, ''); 
+const token = sessionStorage.getItem('memberToken'); 
 const backendUrl = process.env.BACKEND_URL
 const config = axios.create({
     baseURL:backendUrl 
 })
-config.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+config.defaults.headers.common['Authorization'] = `Bearer ${token.replace(/"/g, '')}`;
 
 export default config;
